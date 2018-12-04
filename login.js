0d4189753f29ef81cfacdaeb98c4619db6987aef
Shayne Tuel <shaynetuel@gmail.com>
2018-12-04 18:19:23 -0500

    Updated basic login information.
    Still need to figure out what database we will use to store all of our information in and also need to figure out how to save any login information.

---
+++ login.js
@@ -0,0 +1,16 @@
+// Name and Password from the register-form
+var name = document.getElementById('name').value();
+var pw = document.getElementById('pw').value();
+
+let userPass = new Map();
+// storing input from register-form
+function store() {
+    userPass.set(name, pw);
+}
+
+// check if stored data from register-form is equal to entered data in the   login-form
+
+console.log(userPass.toString());
+
+
+
