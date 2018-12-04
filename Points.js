a64c8359d7d0757d31acf0e67a88ddb26190c8a5
Gabe Skorepa <gskorepa616@gmail.com>
2018-12-04 17:36:39 -0500

    Creating a file for the point system logic

---
+++ Points.js
@@ -0,0 +1,26 @@
+let defaultPointValue = 0;
+let maxPointValue = 20;
+
+let bonusPoints = 5;
+
+
+function Points() {
+    basePoints();
+    timerBonus();
+}
+
+function basePoints() {
+    if (defaultPointValue === 0) {
+        Console.log("Default Point Value is currently set to be: " + defaultPointValue);
+        Console.log("Create a slider to increment the base point value!");
+    } else if (defaultPointValue >= maxPointValue) {
+        Console.log("The maxiumum points per round has been reached");
+    }
+}
+
+function timerBonus() {
+        Console.log("The current bonus points given are: " + bonusPoints);
+        Console.log("Create a slider to adjust the bonus points given max");
+}
+
+Points();
