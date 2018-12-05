7d2a7e01d1906749009f2a0855343fe84342a66a
Gabe Skorepa <gskorepa616@gmail.com>
2018-12-05 17:56:45 -0500

    Still trying to get points and timer to import / export properly

--- points.js
+++ points.js
@@ -1,5 +1,7 @@
 // Create a slider for the number of points you can get for both base points and the timer bonus points
-import {timer} from 'timer.js';
+import Timer from "./timer";
+
+const timer = new Timer();
 
 let basePoints = 10;
 let setBonusPoints = 5;
@@ -7,7 +9,7 @@ let bonusPoints = 0;
 let totalPoints = 0;
 
 function points() {
-    bonusPoints = setBonusPoints / timer.time;
+    bonusPoints = setBonusPoints / timer.time();
     totalPoints = basePoints + bonusPoints;
     console.log(totalPoints);
 }
--- timer.js
+++ timer.js
@@ -1,12 +1,14 @@
 /* This class holds the timer logic for the game */
 /* Create slider for the user to adjust how much time is given */
-export class timer {
-    static time() {
-        let maxTime = 10000; // Must be in Milliseconds...
-        let currentTotal = 0;
-        for (let i = 0; i < maxTime; i++) {
-            currentTotal = i;
+export default class Timer {
+     time() {
+        let now = new Date().getTime();
+        let currentCount = 0;
+        let maxTime = 10000; // Must be in Milliseconds.
+
+        while (currentCount < maxTime) {
+            currentCount = now + 1;
         }
-        return currentTotal;
+        return currentCount;
     }
 }
